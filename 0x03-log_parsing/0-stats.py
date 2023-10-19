e6dcea7682bc08ec2201ed75057383e1d24803e2
Divine Ezelibe <60388882+divineezelibe@users.noreply.github.com>
2023-10-19 06:05:21 +0100

    Updated files

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,71 +1,39 @@
 #!/usr/bin/python3
-""" This is my problem :'v """
+"""
+Log parsing
+"""
+
 import sys
-import re
-import signal
-from collections import OrderedDict
 
+if __name__ == '__main__':
 
-def search_items(line, s):
-    """ Search the items to positionate """
-    regexu = r"\s\d{3}\s\d{1,}"
-    txt = re.search(regexu, line)
-    word = txt.group()
-    word = word[1:]
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-    regexd = r"\d{3}\s"
-    left = re.search(regexd, word)
-
-    code = left.group()
-    code = code[:-1]
-
-    regext = r"\s\d{1,}"
-    right = re.search(regext, word)
-
-    size = right.group()
-    size = size[1:]
-    size = int(size)
-
-    add_code(code, s)
-
-    return size
-
-
-def add_code(code, codes):
-    """ Count the status code """
-    try:
-        codes[code] += 1
-    except KeyError:
-        pass
-
-
-def print_all(stat):
-    """ Print all """
-    stat = OrderedDict(stat)
-
-    for key, value in stat.items():
-        if value is not 0:
-            print("{}: {}".format(key, value))
-
-
-if __name__ == "__main__":
-    status = {"200": 0, "301": 0, "400": 0, "401": 0,
-              "403": 0, "404": 0, "405": 0, "500": 0}
-    file_size = 0
-    i = 0
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
     try:
-        for lines in sys.stdin:
-            file_size += search_items(lines, status)
-
-            if i is not 0 and i % 9 == 0:
-                print("File size: {:d}".format(file_size))
-                print_all(status)
-
-            i += 1
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
     except KeyboardInterrupt:
-        pass
-    finally:
-        print("File size: {:d}".format(file_size))
-        print_all(status)
-        sys.exit(0)
+        print_stats(stats, filesize)
+        raise
